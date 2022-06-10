f3fc0572bc2630c2321bbb5e6af5146185f56294
OM17028 <om17028@ues.edu.sv>
2022-06-10 10:58:29 -0600

    esqueleto main form

--- js/components/mainForm.js
+++ js/components/mainForm.js
@@ -5,9 +5,23 @@ class formularioInicial extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <div>
-          <button>Comprar Ahora</button>
-        </div>
+      <button>create</button>
+      <table BORDER>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">NOMBRE</th>
+        <th scope="col">CAMPO 1</th>
+        <th scope="col">CAMPO 2</th>
+        <th></th>
+      </tr>
+    <tr>
+    <td scope="col">1</th>
+    <td scope="col">ejemplo</th>
+    <td scope="col">campo 1</th>
+    <td scope="col">campo 2</th>
+    <th scope="col"><button>edit</button>|<button>del</button></th>
+    </tr>
+    </table>
       `;
     }
   }
